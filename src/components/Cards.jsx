6f0c5407044f66a973a0512d87b64fd35183e2e3
Shovon96 <fakhruddinshovon@gmail.com>
2023-09-14 22:52:00 +0600

    cart design done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Carts from "./Carts";
-import { FaDollarSign } from 'react-icons/fa';
-import { FaBookOpen } from 'react-icons/fa';
+import { FiDollarSign } from 'react-icons/fi';
+import { FiBookOpen } from 'react-icons/fi';
 
 const Cards = () => {
 
@@ -16,8 +16,7 @@ const Cards = () => {
     }, [])
 
     const handaleSelectCourse = (course) => {
-        // console.log(courses);
-        const isSelectedCourse = selectedCourse.find(item => item.id === course.id) 
+        const isSelectedCourse = selectedCourse.find(item => item.id === course.id)
         setSelectedCourse(isSelectedCourse)
         console.log(isSelectedCourse);
     }
@@ -25,24 +24,26 @@ const Cards = () => {
     return (
         <div>
             <h1 className=" text-5xl text-center font-bold my-4">Course Registration</h1>
-            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-3/4 gap-8 ml-12 my-12">
-                {
-                    courses.map(course =>
-                        <div className="border-2 rounded-lg shadow-lg shadow-gray-400 text-center p-2" key={course.id}>
-                            <img className=" w-full mx-auto my-5" src={course.image} alt="" />
-                            <h2 className=" text-3xl font-bold my-3">{course.title}</h2>
-                            <h4 className=" text-xl p-5">{course.description}</h4>
-                            <div className="flex justify-evenly text-gray-500 my-4 text-2xl font-semibold">
-                                <h5 className="flex items-center gap-2"><FaDollarSign></FaDollarSign>Price: {course.price}</h5>
-                                <h5 className="flex items-center gap-4"><FaBookOpen></FaBookOpen>Credit: {course.credit}hr</h5>
+            <div className="flex">
+                <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-3/4 gap-8 ml-12 my-12">
+                    {
+                        courses.map(course =>
+                            <div className="border-2 rounded-lg shadow-lg shadow-gray-400 p-4" key={course.id}>
+                                <img className=" w-full mx-auto my-5" src={course.image} alt="" />
+                                <h2 className=" text-2xl font-bold my-2">{course.title}</h2>
+                                <h4 className=" text-xl p-5">{course.description}</h4>
+                                <div className="flex justify-evenly text-gray-500 my-2 text-2xl font-semibold">
+                                    <h5 className="flex items-center gap-2"><FiDollarSign></FiDollarSign>Price: {course.price}</h5>
+                                    <h5 className="flex items-center gap-4"><FiBookOpen></FiBookOpen>Credit: {course.credit}hr</h5>
+                                </div>
+                                <button onClick={() => handaleSelectCourse(course)} className="py-3 px-44 mx-auto my-4 bg-blue-500 text-white font-bold capitalize text-2xl rounded-lg">Select</button>
                             </div>
-                            <button onClick={()=>handaleSelectCourse(course)} className="py-4 px-24 my-4 bg-blue-700 text-white font-bold capitalize text-2xl rounded-lg">Select</button>
-                        </div>
-                    )
-                }
-            </div>
+                        )
+                    }
+                </div>
 
-            <Carts></Carts>
+                <Carts></Carts>
+            </div>
         </div>
     );
 };
--- src/components/Carts.jsx
+++ src/components/Carts.jsx
@@ -1,7 +1,17 @@
 const Carts = () => {
     return (
-        <div>
-            <h3>Heello</h3>
+        <div className="ml-20 shadow-lg shadow-gray-500 mt-12 mr-16 p-12 rounded-lg w-1/4">
+            <h2 className="text-2xl font-bold text-blue-600 mb-6">Credit Hour Remaining 7 hr</h2><hr />
+            <div>
+                <h2 className="text-3xl font-bold my-4">Course Name</h2>
+                <ol className="list-decimal my-4">
+                    <li>Course Name</li>
+                    <li>Course Name</li>
+                    <li>Course Name</li>
+                </ol><hr />
+            </div>
+            <h3 className="text-2xl text-gray-600 my-3 font-semibold">Total Credit Hourse:</h3><hr />
+            <h3 className="text-2xl text-gray-600 my-3 font-semibold">Total Price: USD</h3>
         </div>
     );
 };
